41b4fa472ca5e20c094ce1b6db398f556276bb0a
FlaviaCohen <flaviacohen1988@gmail.com>
2023-11-02 17:19:01 -0300

    bottom tabs

--- App.js
+++ App.js
@@ -1,44 +1,58 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+//import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
 import { Ionicons } from "@expo/vector-icons";
 
-const Drawer = createDrawerNavigator();
+//const Drawer = createDrawerNavigator();
+const BottomTab = createBottomTabNavigator();
 
 const App = () => {
-  const { Navigator, Screen } = Drawer;
+  const { Navigator, Screen } = BottomTab; /* Drawer */
+
   return (
     <>
       <StatusBar style="light" />
       <NavigationContainer>
         <Navigator
           screenOptions={{
-            drawerActiveBackgroundColor: "#f0e1ff",
+            /* drawerActiveBackgroundColor: "#f0e1ff",
             drawerActiveTintColor: "#3c0a6b",
-            //drawerStyle: { backgroundColor: "#ccc" },
+            drawerStyle: { backgroundColor: "#ccc" }, */
+            headerStyle: {
+              backgroundColor: "#3c0a6b",
+            },
+            headerTintColor: "white",
+            tabBarActiveTintColor: "#3c0a6b",
           }}
         >
           <Screen
             name="Welcome"
             component={WelcomeScreen}
             options={{
-              headerStyle: { backgroundColor: "#3c0a6b" },
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+              /*  headerStyle: { backgroundColor: "#3c0a6b" },
               headerTintColor: "white",
               drawerLabel: "Welcome Screen",
               drawerIcon: ({ color, size }) => (
                 <Ionicons name="home" color={color} size={size} />
-              ),
+              ), */
             }}
           />
           <Screen
             name="User"
             component={UserScreen}
             options={{
-              drawerIcon: ({ color, size }) => (
+              tabBarIcon: ({ color, size }) => (
                 <Ionicons name="person" color={color} size={size} />
               ),
+              /*  drawerIcon: ({ color, size }) => (
+                <Ionicons name="person" color={color} size={size} />
+              ), */
             }}
           />
         </Navigator>
--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-# Installation 
+# Installation
 
-- Create new project with npx create-expo-app my-app -e with-reanimated
-- npm install @react-navigation/native
-- npx expo install react-native-screens react-native-safe-area-context
-- npm install @react-navigation/drawer
-- npx expo install react-native-gesture-handler 
-- npm start -- --reset-cache
+- Create new project with
+
+```
+npx create-expo-app my-app -e with-reanimated
+```
+
+```
+npm install @react-navigation/native
+```
+
+```
+npx expo install react-native-screens react-native-safe-area-context
+```
+
+```
+npx expo install react-native-gesture-handler
+```
+
+## Drawer
+
+```
+npm install @react-navigation/drawer
+```
+
+```
+npm start -- --reset-cache
+```
+
+## Bottom Tabs
+
+```
+npm install @react-navigation/bottom-tabs
+```
